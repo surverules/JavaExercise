28f38d46439a94f9063acc7914f9fe16e78484fe
surverules <surverules@gmail.com>
2016-11-05 17:05:02 -0500

    Added the URI and Bufferread to read the property file correctly.

--- model/src/main/java/com/backend/PropertyRead.java
+++ model/src/main/java/com/backend/PropertyRead.java
@@ -1,41 +1,45 @@
 package com.backend;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.net.URL;
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Created by surverules on 10/30/2016.
- *
- *      problem:
- *              Not able to find the path --- its not coming correct.
+ * <p>
+ * problem:
+ * Not able to find the path --- its not coming correct.
+ *  Finding:
+ *      1. Use URI to get teh correct location of Path --- it takes out the %20 that appears instead of space
+ *      2. BufferReader.readline to -- read a record.
  */
 public class PropertyRead {
 
 
-    public void readFile() throws IOException {
-        URL path = PropertyRead.class.getClassLoader().getResource("copybook.property");
-        System.out.println("path: " + path);
+    public void readFile() throws IOException, URISyntaxException {
+        URI path = PropertyRead.class.getClassLoader().getResource("copybook.property").toURI();
+        System.out.println("path: " + path.getPath());
         File file = new File(path.getPath());
-        FileReader fileReader =  null;
+        BufferedReader buffReader = null;
 
         try {
-           fileReader = new FileReader(file);
+            buffReader = new BufferedReader(new FileReader(file));
+            for(String record = buffReader.readLine(); record !=null; record = buffReader.readLine()){
+                System.out.println(" Record in the File:" + record);
+            }
         } catch (FileNotFoundException exception) {
             exception.printStackTrace();
         }
 
-        System.out.println(" Record in the File:" + fileReader.read());
     }
 
     public static void main(String[] args) {
-        PropertyRead prop  = new PropertyRead();
+        PropertyRead prop = new PropertyRead();
         try {
             prop.readFile();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
             e.printStackTrace();
         }
     }
