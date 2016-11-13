85ec23ad6fbdd58f014381db5a7b1016c9791acc
surverules <surverules@gmail.com>
2016-11-13 06:41:08 -0600

    Json Reading :  - Array  - Group inside the Json.

--- model/src/main/java/com/backend/ReadJson.java
+++ model/src/main/java/com/backend/ReadJson.java
@@ -9,6 +9,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Iterator;
 
 /**
  * Created by surverules on 11/5/2016.
@@ -20,9 +21,24 @@ public class ReadJson {
         System.out.println("Path: " + uri.getPath());
 
         FileReader fileReader = new FileReader(uri.getPath());
+        //Object creation from the Json
         JSONObject jsonObject = (JSONObject) new JSONParser().parse(fileReader);
         String name = (String) jsonObject.get("MYNAME");
         System.out.println("Name: " + name);
+
+        //Implementation of Array Object
+        JSONArray address = (JSONArray) jsonObject.get("ADDRESS");
+        Iterator i = address.iterator();
+        while (i.hasNext()){
+            JSONObject jsonObject1 = (JSONObject) i.next();
+            Long apt_No = (Long) jsonObject1.get("APT_NO");
+            System.out.println(" Apartment # : " + apt_No);
+        }
+
+        //Group Reading -- Create another object and then again look into that object.
+        JSONObject eduInfo = (JSONObject) jsonObject.get("EDUCATIONAL_INFO");
+        String degree = (String) eduInfo.get("EDUCATION");
+        System.out.println("Education - Degree : " + degree);
 
     }
 
